10f3c5f5a971c9d64ff86d87e3e6f6a83c522516
Bernardo Campos <62954018+bernardocamps@users.noreply.github.com>
2021-02-18 18:37:42 -0300

    adicionada conexão com a Quotes Free API

--- quotes.js
+++ quotes.js
@@ -1,1 +1,8 @@
-/* Teste */
+// Utilizando Quotes Free API
+// https://type.fit/api/quotes
+
+function inspiracao() {
+    fetch('https://type.fit/api/quotes')
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
